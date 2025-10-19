0acc950f38e35f51d45172481cfbbd53fa605960
levidornelas <levidornelas2006@gmail.com>
2025-10-19 20:58:55 -0300

    adicionar guard de jwt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { Pokedex } from './components/pokedex/pokedex';
 import { PokemonFavoritos } from './components/favoritos/favoritos';
 import { GrupoBatalha } from './components/grupo-batalha/grupo-batalha';
 import { AdminComponent } from './components/admin/admin';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: Login },
-  { path: 'cadastro', component: Cadastro },
-  { path: 'pokedex', component: Pokedex },
-  { path: 'favoritos', component: PokemonFavoritos },
-  { path: 'grupo-batalha', component: GrupoBatalha },
-  { path: 'admin', component: AdminComponent },
+  { path: '', component: Login }, // Livre
+  { path: 'cadastro', component: Cadastro }, // Livre
+  { path: 'pokedex', component: Pokedex, canActivate: [authGuard] },
+  { path: 'favoritos', component: PokemonFavoritos, canActivate: [authGuard] },
+  { path: 'grupo-batalha', component: GrupoBatalha, canActivate: [authGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
 ];
